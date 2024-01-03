e0cf98244e3b9abf6e2efdc13870366b570c424b
João Mozelli Neto <joaomozelli@hotmail.com>
2024-01-03 14:34:38 -0300

    create dashboard page

---
+++ src/app/dashboard/layout.tsx
@@ -0,0 +1,13 @@
+import React, { ReactNode } from 'react';
+
+// import { Container } from './styles';
+
+const Dashboard = ({children}: {children: ReactNode}) => {
+  return (
+    <div className="container">
+        <h1>Dashboard</h1>
+    </div>
+  );
+}
+
+export default Dashboard;
